e62bde5de4aa087f5e43a3678c7f145f7b57ecc1
Gustavo Viana Neves <gus.viana1@outlook.com>
2025-08-20 10:42:32 -0300

    Lombok

--- src/main/java/com/person_api/person_api/entity/Person.java
+++ src/main/java/com/person_api/person_api/entity/Person.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.person_api.person_api.entity.enums.GenderEnum;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.io.Serializable;
@@ -16,6 +17,11 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "tb_person")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class Person implements Serializable {
 
     @Id
@@ -39,74 +45,10 @@ public class Person implements Serializable {
     @Column(name = "gender")
     private GenderEnum genderEnum;
 
-    public Person() {
-    }
-
-    public Person(Long id, GenderEnum genderEnum, String cpf, LocalDate dataNascimento, String name) {
-        this.id = id;
-        this.genderEnum = genderEnum;
-        this.cpf = cpf;
-        this.dataNascimento = dataNascimento;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public @NotNull String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf( String cpf) {
-        this.cpf = cpf;
-    }
-
-    public GenderEnum getGender() {
-        return genderEnum;
-    }
-
-    public void setGender(GenderEnum genderEnum) {
-        this.genderEnum = genderEnum;
-    }
-
     public Integer getIdade() {
         if (this.dataNascimento == null) {
             return null;
         }
         return Period.between(this.dataNascimento, LocalDate.now()).getYears();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return Objects.equals(id, person.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
     }
 }
